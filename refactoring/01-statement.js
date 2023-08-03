bda0a4190a6df3099643c0a29a89567037de3bb2
lambert-pro <lambert@jcube.com.cn>
2023-08-03 09:39:14 +0800

    refactoring-提炼计算观众量积分的逻辑

--- refactoring/01-statement.js
+++ refactoring/01-statement.js
@@ -51,27 +51,25 @@ function originStatement(invoice, plays) {
  * 重构后代码
  **/
 function statement(invoice, plays) {
-    let totalAmount = 0
-    let volumeCredits = 0
-    let result = `Statement for ${invoice.customer}\n`
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
     const format = new Intl.NumberFormat('en-US', {
         style: 'currency', currency: 'USD', minimumFractionDigits: 2
-    }).format
+    }).format;
 
     for (let perf of invoice.performances) {
 
         // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0)
-        // add extra credit for every ten comedy attendees
-        if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
+        volumeCredits += volumeCreditsFor(perf);
 
         // print line for this order
-        result += ` 《${playFor(perf).name}》: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`
-        totalAmount += amountFor(perf)
+        result += ` 《${playFor(perf).name}》: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`
-    result += `You earned ${volumeCredits} credits\n`
-    return result
+    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `You earned ${volumeCredits} credits\n`;
+    return result;
 }
 
 // 计算费用
@@ -84,30 +82,37 @@ function statement(invoice, plays) {
  * @returns {number}
  */
 function amountFor(aPerformance) {
-    let result = 0
+    let result = 0;
 
     switch (playFor(aPerformance).type) {
         case 'tragedy':
-          result = 40000
+          result = 40000;
             if (aPerformance.audience > 30) {
-              result += 1000 * (aPerformance.audience - 30)
+              result += 1000 * (aPerformance.audience - 30);
             }
             break
         case 'comedy':
-          result = 30000
+          result = 30000;
             if (aPerformance.audience > 20) {
-              result += 10000 + 500 * (aPerformance.audience - 20)
+              result += 10000 + 500 * (aPerformance.audience - 20);
             }
-          result += 300 * aPerformance.audience
+          result += 300 * aPerformance.audience;
             break
         default:
-            throw new Error(`unknown type: ${playFor(aPerformance).type}`)
+            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
-    return result
+    return result;
 }
 
 function playFor(aPerformance){
     return plays[aPerformance.playID];
+}
+
+function volumeCreditsFor(aPerformance){
+    let volumeCredits;
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
+    if ('comedy' === playFor(aPerformance).type) volumeCredits += Math.floor(aPerformance.audience / 5);
+    return volumeCredits;
 }
 
 let plays = {
@@ -125,6 +130,6 @@ let invoice = {
     ]
 }
 
-console.log('\n')
-console.log(statement(invoice, plays))
-console.log("重构技术就是以微小的步伐修改程序。如果你犯下错误，很容易便可发现它!")
+console.log('\n');
+console.log(statement(invoice, plays));
+console.log("重构技术就是以微小的步伐修改程序。如果你犯下错误，很容易便可发现它!");
